b73508b7b2a14f7416b81742d45a9ebf41329daf
Easygadd <alebiosuisrael99@gmail.com>
2022-03-21 14:41:01 -0700

    Strings value

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - puts a string, followed by a new line
+ * @str: pointer to tge string to print
+ * Return: void
+ */
+
+int _puts(char *str)
+{
+  int i = 0;
+  while (str[i])
+    {
+      _putchar (str[i])
+	i++;
+    }
+  _putchar ('\n');
+}
