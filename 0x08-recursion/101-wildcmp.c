b77a74954cad884e47bacf6a57621c292c635663
Easygadd <alebiosuisrael99@gmail.com>
2022-03-30 02:35:48 -0700

    wild card

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -25,7 +25,7 @@ return (checker(s1, s2, 0, 0, -1));
 int checker(char *s1, char *s2, int a, int b, int wildUsed)
 {
 
-//printf("s1 <%c> a <%i> s2 <%c> b <%i> \n", s1[a], a, s2[b], b);
+printf("s1 <%c> a <%i> s2 <%c> b <%i> \n", s1[a], a, s2[b], b);
 
 if (s1[a] != '\0')
 {	
